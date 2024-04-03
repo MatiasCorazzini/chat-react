e4adee5e61f55e7994a10bf650dfb50b2bbef118
MatiasCorazzini <matiascorazzini@hotmail.com>
2024-04-03 13:05:45 -0300

    first commit

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form";
+
+import { useState, useEffect } from "react";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog"
+
+import { 
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormLabel,
+    FormItem,
+    FormMessage
+} from "@/components/ui/form";
+
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { FileUpload } from "../file-upload";
+ 
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: "Server name is required."
+    }),
+    imageUrl: z.string().min(1, {
+        message: "Server image is required."
+    })
+})
+
+export function InitialModal() {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            imageUrl: "",
+        }
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    async function onSubmit(values: z.infer<typeof formSchema>){
+        console.log(values);
+    }
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <Dialog open={true}>
+            <DialogContent className=" bg-white text-black p-0 overflow-hidden">
+                <DialogHeader className="pt-8 px-6">
+                    <DialogTitle className="text-2xl text-center font-bold">
+                        Prsonaliza tu server 
+                    </DialogTitle>
+                    <DialogDescription className="text-center text-zinc-500">
+                        Dale personalidad a tu server agregandole un nombre y una imagen.
+                        Siempre podras cambiarlos mas tarde.
+                    </DialogDescription>                   
+                </DialogHeader>
+                <Form {...form}>
+                    <form 
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-8"
+                    >
+                        <div className="space-y-8 px-6">
+                            <div className="flex items-center justify-center text-center">
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                            <FormField 
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel
+                                            className="uppercase text-xs font-bolt text-zinc-500
+                                            dark:text-secondary/70"
+                                        >
+                                            Nombre del server
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                disabled={isLoading}
+                                                className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                                                placeholder="Ingrese el nombre del server"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <DialogFooter className="bg-gray-100 px-6 py-4">
+                            <Button 
+                                variant="primary" 
+                                disabled={isLoading}
+                            >
+                                Create
+                            </Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
